e1279414112d705ebadbcc0c61bd65a0edd94df9
Thang1703hrsh <ducthang170301@gmail.com>
2022-04-15 13:18:22 +0700

    login

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,9 +1,11 @@
 const mongoose = require('mongoose')
+const dotenv = require("dotenv");
+dotenv.config();
 
 const connectDB = async() => {
     try {
         const conn = await mongoose.connect(process.env.MONGO_URI)
-
+        
         console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline);
 
     } catch(error)
@@ -14,3 +16,4 @@ const connectDB = async() => {
 }
 
 module.exports = connectDB
+
--- backend/server.js
+++ backend/server.js
@@ -5,9 +5,9 @@ const {errorHandler} = require('./middleware/errorMiddleware')
 const connectDB = require('./config/db')
 const port =process.env.PORT || 5000
 
-connectDB()
-
 const app = express()
+
+connectDB()
 
 app.use(express.json())
 

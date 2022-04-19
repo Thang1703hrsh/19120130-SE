c12c4d1c54c18f3e009085dcab82dc1166566af8
Thang1703hrsh <ducthang170301@gmail.com>
2022-04-19 11:03:38 +0700

    prepare deploy

--- frontend/src/features/auth/authSlice.js
+++ frontend/src/features/auth/authSlice.js
@@ -52,37 +52,37 @@ export const authSlice = createSlice({
   },
   extraReducers: (builder) => {
     builder
-        .addCase(register.pending, (state) => {
-          state.isLoading = true
-        })
-        .addCase(register.fulfilled, (state, action) => {
-          state.isLoading = false
-          state.isSuccess = true
-          state.user = action.payload
-        })
-        .addCase(register.rejected, (state, action) => {
-          state.isLoading = false
-          state.isError = true
-          state.message = action.payload
-          state.user = null
-        })
-        .addCase(login.pending, (state) => {
-          state.isLoading = true
-        })
-        .addCase(login.fulfilled, (state, action) => {
-          state.isLoading = false
-          state.isSuccess = true
-          state.user = action.payload
-        })
-        .addCase(login.rejected, (state, action) => {
-          state.isLoading = false
-          state.isError = true
-          state.message = action.payload
-          state.user = null
-        })
-        .addCase(logout.fulfilled, (state) => {
-          state.user = null
-        })
+      .addCase(register.pending, (state) => {
+        state.isLoading = true
+      })
+      .addCase(register.fulfilled, (state, action) => {
+        state.isLoading = false
+        state.isSuccess = true
+        state.user = action.payload
+      })
+      .addCase(register.rejected, (state, action) => {
+        state.isLoading = false
+        state.isError = true
+        state.message = action.payload
+        state.user = null
+      })
+      .addCase(login.pending, (state) => {
+        state.isLoading = true
+      })
+      .addCase(login.fulfilled, (state, action) => {
+        state.isLoading = false
+        state.isSuccess = true
+        state.user = action.payload
+      })
+      .addCase(login.rejected, (state, action) => {
+        state.isLoading = false
+        state.isError = true
+        state.message = action.payload
+        state.user = null
+      })
+      .addCase(logout.fulfilled, (state) => {
+        state.user = null
+      })
   },
 })
 
